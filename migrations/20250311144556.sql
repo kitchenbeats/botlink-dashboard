e8009244379df442ea1b1b6b972b78724bc6aa93
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-03-13 14:32:56 +0100

    fix: url rewrite order in middleware

--- migrations/20250311144556.sql
+++ migrations/20250311144556.sql
@@ -8,6 +8,3 @@ FROM auth.users;
 REVOKE ALL ON public.auth_users FROM PUBLIC;
 REVOKE ALL ON public.auth_users FROM anon;
 REVOKE ALL ON public.auth_users FROM authenticated;
-
--- Grant SELECT permissions only to supabase_admin role
-GRANT SELECT ON public.auth_users TO supabase_admin;
