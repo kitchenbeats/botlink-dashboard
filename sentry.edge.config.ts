4e50d1d8226c63b135b36f310d65e8594256034e
Ben Fornefeld <50748440+ben-fornefeld@users.noreply.github.com>
2025-05-23 16:59:11 +0200

    Improve Sentry initialization (#69)
    This pr adds better env handling for Sentry config and initializes the
    Sentry clients based on the env

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,15 +6,21 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: 'https://bd99f8144a09171c23efefc41858ecd0@o4505753501564928.ingest.us.sentry.io/4508896141901824',
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE
+    ? parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE)
+    : 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: process.env.NEXT_PUBLIC_SENTRY_DEBUG === '1',
 
   // Disable source maps in development to prevent 404 errors
-  attachStacktrace: process.env.NODE_ENV === 'production',
-  enabled: process.env.NODE_ENV === 'production',
+  attachStacktrace:
+    process.env.NODE_ENV === 'production' &&
+    !!process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enabled:
+    process.env.NODE_ENV === 'production' &&
+    !!process.env.NEXT_PUBLIC_SENTRY_DSN,
 })
