e9527079d6af5c39b7bbd42a9d4e79e5f54972a1
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-07-10 12:01:12 +0200

    Refactor: Remove logger.browser.ts and update logger.ts to clarify edge logger usage

--- src/lib/clients/logger.ts
+++ src/lib/clients/logger.ts
@@ -14,6 +14,8 @@ const loggerImpl = ((): Logger => {
     return require('./logger.node').logger as Logger
   }
 
+  // edge (console) logger will be used in edge or browser runtime
+
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   return require('./logger.edge').logger as Logger
 })()
